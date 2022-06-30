fae38574e228b88548e68a41a24a5221446b19da
spire0859 <tanim0859@gmail.com>
2022-06-30 14:55:38 -0400

    log in page takes u somewhere

--- db/migrate/20220630153855_devise_create_users.rb
+++ db/migrate/20220630153855_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.boolean :admin
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -32,7 +33,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.integer :role, default: 0
 
       t.timestamps null: false
     end
