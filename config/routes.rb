64ce5fbc5e662b8840f1ea95b444e56be006e821
Your Name <you@example.com>
2022-07-01 14:35:26 -0400

    money

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'residential', to: 'pages#residential'
   get 'commercial', to: 'pages#commercial'
   get 'quote', to: 'pages#quote'
+  get 'admin', to: 'pages#admin'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #Route for Homepage
