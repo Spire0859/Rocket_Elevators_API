f00f8c7299f21db3413d9a96ca06743f342c5ddc
MansillaM <matmansilla1992@gmail.com>
2022-07-05 13:05:42 -0400

    last table done

--- db/migrate/20220705092359__create_building_details.rb
+++ db/migrate/20220705092359__create_building_details.rb
@@ -2,6 +2,12 @@ class CreateBuildingDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :buildings_details do |t|
       t.int :buildingId
-      # t.date :information_key   not sure wtf this shouold me
+      t.string :information_key
+      t.text :value
+      # t.date :information_key   not sure wtf this should me
   end
+
+  add_index :settings, :key, :unique => true
+
 end
+
