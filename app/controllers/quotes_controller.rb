4532aabb21da12be52978ed1d7f9a43c13c39cb8
spire0859 <tanim0859@gmail.com>
2022-07-15 20:22:47 -0400

    quote section sends data to freshdesk

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,40 @@ class QuotesController < ApplicationController
     @quotes = Quote.new(quote_params)
     
     if @quotes.save
+      data  = {
+        status: 2, 
+        priority: 1,
+        subject: "#{@quotes.companyName}",
+        email: "#{@quotes.email}",
+        description: " building type: #{@quotes.type_building}, " + " number of appartment: #{@quotes.numApartment}, " + " number of floor: #{@quotes.numFloor}, " + " number of elevator: #{@quotes.numElevator}, " + " message: #{@quotes.message}"
+      }
+  
+      data_json = JSON.generate(data)
+    #  data = JSON.dump({
+    #       "description" => @leads.message,
+    #       "subject" => @leads.nameProject,
+    #       "email" => @leads.email,
+    #       "priority" => 1,
+    #       "status" =>2,
+    #       "cc_emails" => [
+    #         "support@rocketelevators-support.freshdesk.com",
+    #       ]})
+  
+      request = RestClient::Request.execute(
+        method: :post,
+        url: 'https://rocketelevators-support.freshdesk.com/api/v2/tickets',
+        user: "iohhGOT939hTYz9tk2",
+        password: 'X',
+        payload: data_json,
+        headers: {"Content-Type" => 'application/json'}
+      )
+      
+        flash[:notice] = 'Quote created successfully'
+        redirect_to pages_quote_url action: :new
+  
+            
+    end
+
       flash[:notice] = 'Quote created successfully'
       redirect_to pages_quote_url action: :new
     else
