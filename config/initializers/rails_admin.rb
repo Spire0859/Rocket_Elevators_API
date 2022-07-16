7d8b48b099fdd8490275898e00976485c25d1c4e
spire0859 <tanim0859@gmail.com>
2022-07-15 20:10:46 -0400

    aesthetic changes and freshdesk sends all the correct info

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -66,6 +66,7 @@ RailsAdmin.config do |config|
   
   config.navigation_static_links = {
     'Play Briefing' => 'rails_admin/data/playbriefing'
+    'Google Maps' => 'pages/customers_location'
     
   }
 
