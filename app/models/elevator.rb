7405ca39771b72eca692c3b9e80e2245f6e32518
CAlexisAA <c.alexisalm.a@gmail.com>
2022-07-19 09:30:23 -0400

    made few changes for Twilio / Slack

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -7,16 +7,20 @@ class Elevator < ApplicationRecord
     after_update do
         elevator = self.status_changed?
         notifier = Slack::Notifier.new 'https://hooks.slack.com/services/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', channel: "#elevator_operations", username: "calexis_slack_api"
-        notifier.ping 'The Elevator = ' + self.model + ' w Serial Number = ' + self.serial_number + ' was : ' + self.status + ' and now : ' + self.status                                                                              
+        notifier.ping 'The Elevator = ' + self.model + ' w Serial Number = ' + self.serial_number + ' was Active and now : ' + self.status                                                                              
     end
     # /SLACK #
     
     # TWILIO # 
     after_update :send_sms
 
-     def send_sms        
-        
+     def send_sms
+        puts "hello -------------------------********************"
+        puts self.status
+        puts "end***********************************"
+        # if self.status_changed?
             if self.status == 'Inactive'
+                puts "******************toto////////////////////////"
                 account_sid = 'ACa60c0679e85c76d473a3e72d0638ed79'
                 auth_token = '7d57e4b3a2dfb07e4debbcb0d12a6b03'
                 @client = Twilio::REST::Client.new(account_sid, auth_token)
@@ -25,14 +29,28 @@ class Elevator < ApplicationRecord
                 to = '+15148230596'
                 message = @client.messages.create(
                     body: 'Elevator needs Attention!',
+                    #   from: from,a
                     from: from,
+                    #   to: to
                     to: to
                 )
             end
     end
 end
 
- # /TWILIO #
+
+
+    #     if self.status == 'Inactive'
+    #         message = @client.messages
+    #           .create(
+    #              body: 'Elevator is Inactive',
+    #              from: '+19854018996',
+    #              to: '+15148230596'
+    #             )
+          
+    #     end
+    # end
+     # /TWILIO #
 
 
 
